93978239d91a625f13885b13cd57890812099763
xiaolan1001 <37076254+xiaolan1001@users.noreply.github.com>
2018-06-14 17:22:03 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/xiaolan/coolweather/util/Utility.java
+++ app/src/main/java/com/xiaolan/coolweather/util/Utility.java
@@ -2,9 +2,11 @@ package com.xiaolan.coolweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.xiaolan.coolweather.db.City;
 import com.xiaolan.coolweather.db.County;
 import com.xiaolan.coolweather.db.Province;
+import com.xiaolan.coolweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -80,5 +82,20 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
